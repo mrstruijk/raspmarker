ae5165391935f4ab2d8388c779ed53c7212d12d8
mrstruijk <35394193+mrstruijk@users.noreply.github.com>
2025-09-08 18:39:53 +0200

    Cleanup

--- MarkerOut.py
+++ MarkerOut.py
@@ -4,22 +4,23 @@ onRPi = (platform.system() == 'Linux')
 
 if onRPi:
     import RPi.GPIO as GPIO
+
     GPIO.setmode(GPIO.BCM)
-    
+
     PIN_DATA = 26
     PIN_LATCH = 19
     PIN_CLOCK = 13
-    
+
     GPIO.setup(PIN_DATA, GPIO.OUT)
     GPIO.setup(PIN_LATCH, GPIO.OUT)
     GPIO.setup(PIN_CLOCK, GPIO.OUT)
-    
-    
+
+
 class MarkerOut():
-    
+
     def __init__(self):
         super().__init__()
-    
+
     def sendMarker(self, value):
         if onRPi:
             GPIO.output(PIN_LATCH, 0)
@@ -27,6 +28,4 @@ class MarkerOut():
                 GPIO.output(PIN_DATA, (value >> x) & 1)
                 GPIO.output(PIN_CLOCK, 1)
                 GPIO.output(PIN_CLOCK, 0)
-            GPIO.output(PIN_LATCH, 1) 
-            
-            
+            GPIO.output(PIN_LATCH, 1)
--- README.md
+++ README.md
@@ -1,17 +1,16 @@
-MarkerBox 
-
+MarkerBox
 
 Python 3.7.3
 
 Requirements libraries:
 Kivy==1.11.1
-kivy-garden,.graph===0.4.dev0
+kivy-garden.graph===0.4.dev0
 
 RPi.GPIO ... (when using Marker Input and Raspberry Pi)
 
-Run: 
+Run:
 
-When using random markers: 
+When using random markers:
 
 python kivyGUI.py 0
 
