94d598bc12d6cdaa908f9721ad46cb178d94e8a2
mrstruijk <35394193+mrstruijk@users.noreply.github.com>
2025-11-03 15:44:22 +0100

    Other files

---
+++ Other files/mqtt-subscribe-to-all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# /usr/local/bin/mqtt-subscribe-to-all.sh
+# Install ts via `sudo apt install moreutils`
+# Store this file at `/usr/local/bin/mqtt-subscribe-to-all.sh`
+# Make it executable `sudo chmod +x /usr/local/bin/mqtt-subscribe-to-all.sh`
+# View stored logs via `cat /var/log/mqtt-subscribe-to-all.log`
+
+mosquitto_sub -h localhost -t '#' -v -u SOLO -P SOLO1B11 \
+| ts '[%Y-%m-%d %H:%M:%S] RECEIVED:' \
+| tee -a /var/log/mqtt-subscribe-to-all.log
