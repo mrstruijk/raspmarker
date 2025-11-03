348c0e02a71a015186591cba9eb2d7714cfff922
mrstruijk <35394193+mrstruijk@users.noreply.github.com>
2025-11-03 16:23:29 +0100

    Update mqtt-subscribe-to-all.sh

--- service_files/mqtt-subscribe-to-all.sh
+++ service_files/mqtt-subscribe-to-all.sh
@@ -6,4 +6,14 @@
 # Make it executable `sudo chmod +x /usr/local/bin/mqtt-subscribe-to-all.sh`
 # View stored logs via `cat /var/log/mqtt-subscribe-to-all.log`
 
-mosquitto_sub -h localhost -t '#' -v -u SOLO -P SOLO1B11 | ts '[%Y-%m-%d %H:%M:%S] RECEIVED:' | tee -a /var/log/mqtt-subscribe-to-all.log
+LOGFILE="/var/log/mqtt-subscribe-to-all.log"
+
+# ensure log file exists and is writable
+if [ ! -f "$LOGFILE" ]; then
+    sudo touch "$LOGFILE"
+    sudo chown $(whoami):$(whoami) "$LOGFILE"
+fi
+
+mosquitto_sub -h localhost -t '#' -v -u SOLO -P SOLO1B11 \
+| ts '[%Y-%m-%d %H:%M:%S] RECEIVED:' \
+| tee -a "$LOGFILE"
