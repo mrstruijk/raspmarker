338ca38437c2a0230154036f3b7ce0145ebf2648
mrstruijk <35394193+mrstruijk@users.noreply.github.com>
2025-11-03 16:41:12 +0100

    Update mqtt-subscribe-to-all.sh

--- service_files/mqtt-subscribe-to-all.sh
+++ service_files/mqtt-subscribe-to-all.sh
@@ -4,16 +4,22 @@
 # Install ts via `sudo apt install moreutils`
 # Store this file at `/usr/local/bin/mqtt-subscribe-to-all.sh`
 # Make it executable `sudo chmod +x /usr/local/bin/mqtt-subscribe-to-all.sh`
-# View stored logs via `cat /var/log/mqtt-subscribe-to-all.log`
+# View stored logs via `cat ~/.local/share/mqtt-logs/mqtt-subscribe-to-all.log` (or whatever is set at LOGFILE)
 
-LOGFILE="/var/log/mqtt-subscribe-to-all.log"
+LOGFILE="$HOME/.local/share/mqtt-logs/mqtt-subscribe-to-all.log"
+LOGINFO="[%Y-%m-%d %H:%M:%S] RECEIVED:"
 
-# ensure log file exists and is writable
+# ensure log directory exists
+mkdir -p "$(dirname "$LOGFILE")"
+
+# ensure log file exists
 if [ ! -f "$LOGFILE" ]; then
-    sudo touch "$LOGFILE"
-    sudo chown $(whoami):$(whoami) "$LOGFILE"
+    touch "$LOGFILE"
 fi
 
-mosquitto_sub -h localhost -t '#' -v -u SOLO -P SOLO1B11 \
-| ts '[%Y-%m-%d %H:%M:%S] RECEIVED:' \
-| tee -a "$LOGFILE"
+# ensure current user can write to it
+if [ ! -w "$LOGFILE" ]; then
+    chown "$(whoami):$(whoami)" "$LOGFILE"
+fi
+
+mosquitto_sub -h localhost -t '#' -v -u SOLO -P SOLO1B11 | ts "$LOGINFO" | tee -a "$LOGFILE"
